41f7be168eabfc5b5b0cd5e5aeed46fa04af66e7
CasDragon <redyugi112@gmail.com>
2024-08-17 14:25:56 -0400

    x

--- RedditorClass/Components/DRComponent.cs
+++ RedditorClass/Components/DRComponent.cs
@@ -9,26 +9,36 @@ using Kingmaker.UnitLogic.FactLogic;
 using Kingmaker.Items;
 using Kingmaker.Enums.Damage;
 using Kingmaker.PubSubSystem;
+using UnityEngine;
 
 namespace RedditorClass.Components
 {
-    [AllowedOn(typeof(BlueprintUnitFact), true)]
-    [AllowedOn(typeof(BlueprintUnit), true)]
+    [AllowedOn(typeof(BlueprintFact), false)]
     [AllowMultipleComponents]
     [TypeId("A9675332-F963-4F5F-AA97-7E6E85E0D8D1")]
     internal class DRComponent :  AddDamageResistanceBase
     {
         public StatType Stat = StatType.Charisma;
-        public bool UsePool = false;
+        public bool m_IsStackable = true;
 
         public DamageEnergyTypeFlag m_ExcludedTypes;
-        public override bool Bypassed(DRComponent.ComponentRuntime runtime, BaseDamage damage, ItemEntityWeapon weapon)
+        public override bool Bypassed(AddDamageResistanceBase.ComponentRuntime runtime, BaseDamage damage, ItemEntityWeapon weapon)
         {
             return false;
         }
-        public override int CalculateValue(ComponentRuntime runtime)
+        public override int CalculateValue(AddDamageResistanceBase.ComponentRuntime runtime)
         {
-            return runtime.Owner.Stats.GetStat(this.Stat).CalculatePermanentValue() / 2 - 5;
+            Main.log.Log("Calculating DR Value ");
+            var x = runtime.Owner.Stats.GetStat(this.Stat).CalculatePermanentValue() / 2 - 5;
+            Main.log.Log("DR Value is - " + x.ToString());
+            return x;
+        }
+        public override bool IsStackable
+        {
+            get
+            {
+                return this.m_IsStackable;
+            }
         }
     }
 }
